56c23f58b836751bc34272f5377a4eb2d970e908
WHUrology <107327908+WHUrology@users.noreply.github.com>
2025-11-26 07:31:25 +0800

    Update diy-part1.sh

--- padavanonly/Redmi-AX6000/diy-part1.sh
+++ padavanonly/Redmi-AX6000/diy-part1.sh
@@ -16,9 +16,15 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
-echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
-echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+#echo 'src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 #echo 'src-git messense https://github.com/messense/aliyundrive-webdav' >>feeds.conf.default
 #echo 'src-git coolsnowwolf https://github.com/coolsnowwolf/luci.git' >>feeds.conf.default
 #echo 'src-git xiaoqingfeng https://github.com/xiaoqingfengATGH/feeds-xiaoqingfeng.git' >>feeds.conf.default
+# 移除 openwrt feeds 自带的核心库
+        rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+        git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+# 移除 openwrt feeds 过时的luci版本
+        rm -rf feeds/luci/applications/luci-app-passwall
+        git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
